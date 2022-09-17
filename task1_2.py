399c2c59c036ebba7a86c2e831900abb64fd87bc
Deepika9103 <102160852+Deepika9103@users.noreply.github.com>
2022-09-17 17:57:45 +0530

    Task1-Slicing

---
+++ task1_2.py
@@ -0,0 +1,5 @@
+creepy_doll=['red_light','green_light','red_light','you_got_shot','green_light','you_got_shot','you_got_shot','green_light','you_got_shot','red_light','green_light','red_light','you_got_shot','green_light','red_light','red_light','green_light','you_got_shot','red_light','you_got_shot']
+l=len(creepy_doll)
+next_game=creepy_doll[1:l-1:3]
+print(next_game)
+
